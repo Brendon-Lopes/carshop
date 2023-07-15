a167cb6e16373e89dc14026009ce801ff899cfda
Brendon <brendon.lopes.21@gmail.com>
2023-07-15 15:26:53 -0300

    chore: update IRepository
    GetById method can return null

--- server/Carshop.Domain/Interfaces/IRepository.cs
+++ server/Carshop.Domain/Interfaces/IRepository.cs
@@ -2,7 +2,7 @@ namespace Carshop.Domain.Interfaces;
 
 public interface IRepository<TEntity> where TEntity : class
 {
-    TEntity GetById(Guid id);
+    TEntity? GetById(Guid id);
     IEnumerable<TEntity> GetAll();
     void Save(TEntity entity);
     void Update(TEntity entity);
