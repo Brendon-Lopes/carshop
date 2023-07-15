9598dba40e5d3c15ed52e1757bbf6a9dd2245e30
Brendon <brendon.lopes.21@gmail.com>
2023-07-15 20:03:28 -0300

    feat: add BrandMapper

---
+++ server/Carshop.API/Mappers/BrandMapper.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Carshop.Application.DTOs;
+using Carshop.Domain.Models;
+
+namespace Carshop.API.Mappers;
+
+public class BrandMapper : Profile
+{
+    public BrandMapper()
+    {
+        CreateMap<BrandDTO, Brand>().ReverseMap();
+    }
+}
