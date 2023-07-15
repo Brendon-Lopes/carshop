0a90a16457a8587dbde5129dd364c83c3b12aa0f
Brendon <brendon.lopes.21@gmail.com>
2023-07-15 20:03:10 -0300

    chore: update builder services
    add dependency injections and automapper

--- server/Carshop.API/Program.cs
+++ server/Carshop.API/Program.cs
@@ -1,25 +1,30 @@
+using Carshop.Application;
+using Carshop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // dependency injection
+    builder.Services
+        .AddInfrastructure(builder.Configuration)
+        .AddApplication();
+
+    builder.Services.AddAutoMapper(typeof(Program).Assembly);
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
