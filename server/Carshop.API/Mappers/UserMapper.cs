0f17fd2c46709133b85cdd4d36cbe541b82aafa0
Brendon <brendon.lopes.21@gmail.com>
2023-07-16 17:57:39 -0300

    feat: add user mapping configs

---
+++ server/Carshop.API/Mappers/UserMapper.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Carshop.Application.DTOs;
+using Carshop.Domain.Models;
+
+namespace Carshop.API.Mappers;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+         CreateMap<UserDTO, User>().ReverseMap();
+    }
+}
