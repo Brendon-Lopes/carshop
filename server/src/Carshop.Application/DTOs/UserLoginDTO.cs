741fb934166ac803bb7d687e0365f3124e15443e
Brendon <brendon.lopes.21@gmail.com>
2023-07-17 02:00:57 -0300

    feat: add validation to UserLoginDTO

--- server/src/Carshop.Application/DTOs/UserLoginDTO.cs
+++ server/src/Carshop.Application/DTOs/UserLoginDTO.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Carshop.Application.DTOs;
 
 public class UserLoginDTO
 {
+    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
