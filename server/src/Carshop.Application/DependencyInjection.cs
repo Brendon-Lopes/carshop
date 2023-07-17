982dd199d67dcca9281081c6532ad651667ec60a
Brendon <brendon.lopes.21@gmail.com>
2023-07-17 00:23:48 -0300

    chore: update dependency injections

--- server/src/Carshop.Application/DependencyInjection.cs
+++ server/src/Carshop.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using Carshop.Application.Interfaces.Authentication;
 using Carshop.Application.Interfaces.Brand;
+using Carshop.Application.Interfaces.User;
 using Carshop.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +11,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IBrandService, BrandService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
 
         return services;
     }
