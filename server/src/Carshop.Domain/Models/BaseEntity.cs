fda42fd4915f6af301a4d596d3e961e26d2422d9
Brendon <brendon.lopes.21@gmail.com>
2023-07-17 23:15:52 -0300

    chore: update BaseEntity
    add setter to CreatedAt property to set it up on dbcontext

--- server/src/Carshop.Domain/Models/BaseEntity.cs
+++ server/src/Carshop.Domain/Models/BaseEntity.cs
@@ -6,6 +6,6 @@ public class BaseEntity
 {
     [Key]
     public Guid Id { get; private set; } = Guid.NewGuid();
-    public DateTime CreatedAt { get; private set; }
+    public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
