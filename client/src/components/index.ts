592af7e1e58fcf661aef03848a2c5e0aa7d11cfc
Brendon <brendon.lopes.21@gmail.com>
2023-07-19 00:52:40 -0300

    feat: add CarCard component

---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+import { Navbar } from "./Navbar";
+import { SearchBar } from "./SearchBar";
+import { CarCard } from "./CarCard";
+import { SideBar } from "./SideBar";
+
+export { Navbar, SearchBar, CarCard, SideBar };
