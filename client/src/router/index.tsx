847465292f5d65592d519c449d2067e0be8e57cc
Brendon <brendon.lopes.21@gmail.com>
2023-07-18 21:05:50 -0300

    feat: add Login page

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "@pages/Home/";
+import { Login } from "@pages/Login";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register-car" element={<Home />} />
         <Route path="/edit-car" element={<Home />} />
         <Route path="*" element={<h1>404 Page Not Found</h1>} />
