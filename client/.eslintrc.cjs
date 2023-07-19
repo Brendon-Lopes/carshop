ce145fb7424e249c1361a191fd5f0a3322907b3a
Brendon <brendon.lopes.21@gmail.com>
2023-07-18 22:01:52 -0300

    chore: update lint setup

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -25,5 +25,6 @@ module.exports = {
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
