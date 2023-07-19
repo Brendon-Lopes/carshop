8cbad865c10990730a0f32c372442e6dfaaf63a4
Brendon <brendon.lopes.21@gmail.com>
2023-07-18 21:04:59 -0300

    chore: add paths for absolute imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,8 @@ export default defineConfig({
       "@components": "/src/components",
       "@pages": "/src/pages",
       "@utils": "/src/utils",
+      "@services": "/src/services",
+      "@validations": "/src/validations",
     },
   },
 });
