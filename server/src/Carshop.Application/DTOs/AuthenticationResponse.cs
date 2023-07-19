f18007f7e7f1f0e30753bfdcb6bf02c71a39806b
Brendon <brendon.lopes.21@gmail.com>
2023-07-18 21:47:43 -0300

    chore: add user Role on AuthenticationResponse

--- server/src/Carshop.Application/DTOs/AuthenticationResponse.cs
+++ server/src/Carshop.Application/DTOs/AuthenticationResponse.cs
@@ -7,4 +7,5 @@ public class AuthenticationResponse
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Token { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
 }
