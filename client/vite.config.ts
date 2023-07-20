d5271eff17e9110ccda02150084c6d83ace6e801
Brendon <brendon.lopes.21@gmail.com>
2023-07-20 01:13:15 -0300

    chore: update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
