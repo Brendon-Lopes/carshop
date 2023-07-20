3139e31b9cef5f5941ccc5a6bd2cba66e3bfac98
Brendon <brendon.lopes.21@gmail.com>
2023-07-19 22:15:36 -0300

    feat: add option to create a brand when creating or editing a car

--- client/src/components/RegisterCarForm/index.tsx
+++ client/src/components/RegisterCarForm/index.tsx
@@ -5,21 +5,29 @@ import * as brandService from "src/services/brand.service";
 import * as carService from "src/services/car.service";
 import { useCookies } from "react-cookie";
 import { useNavigate, useLocation } from "react-router-dom";
+import ReactModal from "react-modal";
 import {
   createCarResolver,
   type ICreateCarFormData,
 } from "src/validations/car.validation";
 
-export function RegisterCarForm() {
+export const RegisterCarForm = () => {
   const location = useLocation();
   const state = location.state as { editMode: boolean; car: ICar };
   const editMode = state.editMode;
 
   const [brands, setBrands] = useState<IBrand[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [newBrandName, setNewBrandName] = useState<string>("");
+  const [newBrandLengthError, setNewBrandLengthError] =
+    useState<boolean>(false);
 
   const [selectedBrand, setSelectedBrand] = useState<string>(
-    editMode ? state.car.brandId : "",
+    editMode ? state.car.brandId : ""
   );
+
+  const [triggerReloadBrands, setTriggerReloadBrands] =
+    useState<boolean>(false);
 
   const [cookies] = useCookies(["token"]);
 
@@ -64,6 +72,30 @@ export function RegisterCarForm() {
     });
   };
 
+  const handleCreateBrand = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (newBrandName.length < 2) {
+      setNewBrandLengthError(true);
+      return;
+    }
+
+    void brandService
+      .createBrand(newBrandName, cookies.token as string)
+      .then((res) => {
+        if (res === false) {
+          alert("Erro ao criar marca!");
+          return;
+        }
+
+        alert("Marca criada com sucesso!");
+        setIsModalOpen(false);
+        setNewBrandName("");
+        setNewBrandLengthError(false);
+        setTriggerReloadBrands((prev) => !prev);
+      });
+  };
+
   useEffect(() => {
     void brandService.getAllBrands().then((res) => {
       setBrands(res as unknown as IBrand[]);
@@ -77,129 +109,242 @@ export function RegisterCarForm() {
     setValue("brand", editMode ? state.car.brandId : "");
   }, [editMode]);
 
+  useEffect(() => {
+    void brandService.getAllBrands().then((res) => {
+      setBrands(res as unknown as IBrand[]);
+    });
+  }, [triggerReloadBrands]);
+
   return (
-    <form
-      onSubmit={handleSubmit(editMode ? onEdit : onSubmit)}
-      className="w-full sm:w-full md:w-3/4 lg:w-1/2 m-auto mt-6"
-    >
-      <div className="mb-6">
-        <label
-          htmlFor="name"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Nome / Descrição
-        </label>
-        <input
-          type="text"
-          {...register("name")}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        />
-        {errors.name !== undefined && (
-          <p className="error-message">{errors.name.message}</p>
-        )}
-      </div>
-
-      <div className="mb-6">
-        <label
-          htmlFor="model"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Modelo
-        </label>
-        <input
-          type="text"
-          {...register("model")}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        />
-        {errors.model !== undefined && (
-          <p className="error-message">{errors.model.message}</p>
-        )}
-      </div>
-
-      <div className="mb-6">
-        <label
-          htmlFor="brand"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Marca
-        </label>
-        <select
-          {...register("brand")}
-          value={editMode ? selectedBrand : ""}
-          onChange={(e) => setSelectedBrand(e.target.value)}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        >
-          <option disabled value="">
-            Selecione uma marca
-          </option>
-          {brands.map((brand) => (
-            <option key={brand.id} value={brand.id}>
-              {brand.name}
-            </option>
-          ))}
-        </select>
-        {errors.brand !== undefined && (
-          <p className="error-message">{errors.brand.message}</p>
-        )}
-      </div>
-
-      <div className="mb-6">
-        <label
-          htmlFor="year"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Ano
-        </label>
-        <input
-          type="number"
-          {...register("year")}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        />
-        {errors.year !== undefined && (
-          <p className="error-message">{errors.year.message}</p>
-        )}
-      </div>
-
-      <div className="mb-6">
-        <label
-          htmlFor="price"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Preço
-        </label>
-        <input
-          type="number"
-          {...register("price")}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        />
-        {errors.price !== undefined && (
-          <p className="error-message">{errors.price.message}</p>
-        )}
-      </div>
-
-      <div className="mb-6">
-        <label
-          htmlFor="imageUrl"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          URL da imagem
-        </label>
-        <input
-          type="text"
-          {...register("imageUrl")}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        />
-        {errors.imageUrl !== undefined && (
-          <p className="error-message">{errors.imageUrl.message}</p>
-        )}
-      </div>
-
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+    <>
+      <form
+        onSubmit={handleSubmit(editMode ? onEdit : onSubmit)}
+        className="w-full sm:w-full md:w-3/4 lg:w-1/2 m-auto mt-6"
       >
-        {editMode ? "Editar" : "Cadastrar"}
-      </button>
-    </form>
+        <div className="mb-6">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Nome / Descrição
+          </label>
+          <input
+            type="text"
+            {...register("name")}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          />
+          {errors.name !== undefined && (
+            <p className="error-message">{errors.name.message}</p>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="model"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Modelo
+          </label>
+          <input
+            type="text"
+            {...register("model")}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          />
+          {errors.model !== undefined && (
+            <p className="error-message">{errors.model.message}</p>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="brand"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Marca
+          </label>
+          <section className="grid grid-cols-3 gap-4">
+            <select
+              {...register("brand")}
+              value={selectedBrand}
+              onChange={(e) => setSelectedBrand(e.target.value)}
+              className="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
+            >
+              <option disabled value="">
+                Selecione uma marca
+              </option>
+              {brands.map((brand) => (
+                <option key={brand.id} value={brand.id}>
+                  {brand.name}
+                </option>
+              ))}
+            </select>
+
+            <button
+              type="button"
+              className="col-span-1 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-400 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center"
+              onClick={() => setIsModalOpen(true)}
+            >
+              Cadastrar nova marca
+            </button>
+          </section>
+          {errors.brand !== undefined && (
+            <p className="error-message">{errors.brand.message}</p>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="year"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Ano
+          </label>
+          <input
+            type="number"
+            {...register("year")}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          />
+          {errors.year !== undefined && (
+            <p className="error-message">{errors.year.message}</p>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="price"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Preço
+          </label>
+          <input
+            type="number"
+            {...register("price")}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          />
+          {errors.price !== undefined && (
+            <p className="error-message">{errors.price.message}</p>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="imageUrl"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            URL da imagem
+          </label>
+          <input
+            type="url"
+            {...register("imageUrl")}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          />
+          {errors.imageUrl !== undefined && (
+            <p className="error-message">{errors.imageUrl.message}</p>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+        >
+          {editMode ? "Editar" : "Cadastrar"}
+        </button>
+
+        <button
+          type="button"
+          className="ml-4 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          onClick={() => navigate("/")}
+        >
+          Cancelar
+        </button>
+      </form>
+
+      <ReactModal
+        isOpen={isModalOpen}
+        className="
+          px-10
+          py-7
+          bg-slate-100
+          absolute
+          top-1/2
+          left-1/2
+          transform
+          -translate-x-1/2
+          -translate-y-1/2
+          rounded-lg
+          min-w-[30rem]
+        "
+        overlayClassName="bg-black bg-opacity-50 absolute top-0 left-0 w-full h-full"
+      >
+        <form onSubmit={handleCreateBrand}>
+          <h2 className="block mb-2 text-sm font-medium text-gray-900">
+            Criar nova marca
+          </h2>
+
+          <input
+            className="
+            bg-gray-50
+            border
+            border-gray-300
+            text-gray-900
+            text-sm rounded-lg
+            focus:ring-blue-500
+            focus:border-blue-500
+            block
+            w-full
+            p-2.5
+            mb-2
+          "
+            placeholder="Nome da marca"
+            type="text"
+            value={newBrandName}
+            onChange={(e) => setNewBrandName(e.target.value)}
+          />
+
+          {newBrandLengthError && (
+            <p className="error-message mb-4">
+              O nome da marca deve ter no mínimo 2 caracteres
+            </p>
+          )}
+
+          <section className="mt-6">
+            <button
+              type="submit"
+              className="
+              text-white
+              bg-blue-700
+              hover:bg-blue-800
+              focus:ring-4
+              focus:outline-none
+              focus:ring-blue-300
+              font-medium
+              rounded-lg
+              text-sm
+              w-full
+              sm:w-auto
+              px-5
+              py-2.5
+              text-center
+              mr-2
+            "
+            >
+              Criar marca
+            </button>
+
+            <button
+              className="text-white bg-red-700 hover:bg-blredue-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+              type="button"
+              onClick={() => {
+                setIsModalOpen(false);
+                setNewBrandName("");
+                setNewBrandLengthError(false);
+              }}
+            >
+              Cancelar
+            </button>
+          </section>
+        </form>
+      </ReactModal>
+    </>
   );
-}
+};
